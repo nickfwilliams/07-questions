d96c7b70af754fbaff35f9bfb70ecca4248a9b42
Nick Williams <nick.williams@bristol.ac.uk>
2024-02-23 16:00:29 +0000

    Make initial commit

---
+++ setup/app.js
@@ -0,0 +1,12 @@
+//using selectors inside the element
+
+// traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+// const question = document.querySelector(".question");
+
+// btns.forEach((button) => {
+// 	button.addEventListener("click", function (e) {
+// 		const question = e.currentTarget.parentElement.parentElement;
+// 		question.classList.toggle("show-text");
+// 	});
+// });
